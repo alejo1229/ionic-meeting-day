9a8118e9777a6a81c0786543131251ff60a45baf
alejo1229 <alejandroortega222@gmail.com>
2023-02-02 09:32:35 -0500

    primer commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+//export const URL = "http://127.0.0.1:8000";
+export const URL = "https://recursing-cray.212-227-229-244.plesk.page";
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCfjnV0-sPAG3xmQ6Tp7SlOXQb-UZz5JeI',
+    authDomain: 'meetingday-3a5c8.firebaseapp.com',
+    projectId: 'meetingday-3a5c8',
+    appId: '527990876988',
+};  
